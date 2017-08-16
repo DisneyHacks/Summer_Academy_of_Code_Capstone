7507af3d547f30fd1a6d26472357b684355686d7
Jocelyn Rodriguez <jocelynrodriguez@Jocelyns-MacBook-Air.local>
2017-08-16 15:16:09 -0700

    content script updates

---
+++ disney-script.js
@@ -0,0 +1,8 @@
+//content script that will iteract with page
+console.log("disney content script: begin...");
+
+
+//get the google logo
+// console.log("removing logo");
+// var logo = $('#hplogo');
+// logo.remove();
