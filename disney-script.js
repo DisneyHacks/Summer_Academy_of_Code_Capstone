2bdd7177a469a3455b35e7756f90b15e6a008786
Jocelyn Rodriguez <jocelynrodriguez@Jocelyns-MacBook-Air.local>
2017-08-16 15:39:19 -0700

    update disney-script.js

--- disney-script.js
+++ disney-script.js
@@ -2,10 +2,13 @@
 console.log("disney content script: begin...");
 
 
-//get the google logo
-// console.log("removing logo");
-// var logo = $('#hplogo');
-// logo.remove();
-var logos = []; 
-logos = ['logo1.png','logo2.png','logo3.png', 'logo4.png', 'logo5.png', 
-'logo6.png', 'logo7.png', 'logo8.png','logo9.png','logo10.png','logo11.png']; 
+//put all possible logo images in an array
+var logos = [];
+logos = ['logo1.png','logo2.png','logo3.png', 'logo4.png', 'logo5.png',
+'logo6.png', 'logo7.png', 'logo8.png','logo9.png','logo10.png','logo11.png'];
+
+//get a random image from our array
+var image = function(){return logos[Math.floor(Math.random()*logos.length)]};
+console.log(image());
+
+console.log("disney content script: ...end");
