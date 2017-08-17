b31c186261cf7ef18e91b68781b0197a8e4ae6bf
dianax3dh <diananhoang@gmail.com>
2017-08-16 19:52:04 -0700

    update

--- disney-script.js
+++ disney-script.js
@@ -6,17 +6,19 @@ var googlePathName = "/";
 if (window.location.pathname === newTabPathName || window.location.pathname === googlePathName){
 //IMAGES URL HERE
 var logo1 = "https://lh3.googleusercontent.com/RVtwSeu8ZWL-IjY0o6IVDzjQ01BEqNBGUFn0n73enrORfVFmpK6AGHFDWZ2nYKMGXIaI4FXBHqMvPah36UvPiTq3b2i0EZ1uIR377Aw=s660"; 
-//var logo2 = 
+var logo2 = "https://rebeccanicholeratliff.files.wordpress.com/2013/01/disney-facebookcover.jpg"; 
+var logo3 = "https://s-media-cache-ak0.pinimg.com/736x/f4/6c/4a/f46c4af8bd526f632be370f3d875f139--facebook-profile-facebook-timeline.jpg"; 
+var logo4 = "https://i1.wp.com/holidaysandobservances.net/wp-content/uploads/2015/09/tumblr_muxvjfmnun1rf73xqo10_1280.png"; 
 //put all possible logo images in an array
-// var logos = [];
-// logos = ['logo1.jpg','logo2.png','logo3.jpg', 'logo4.png', 'logo5.jpg',
+var logos = [];
+logos = [logo1, logo2, logo3, logo4]//, 'logo5.jpg',
 // 'logo6.jpg', 'logo7.png', 'logo8.png','logo9.png','logo10.png','logo11.png'];
 
 // var imageDiv = document.getElementById('lga'); 
 // imageDiv.appendChild(images); 
 
 //get a random image from our array
-//var image = function(){return logos[Math.floor(Math.random()*logos.length)]};
+var image = function(){return logos[Math.floor(Math.random()*logos.length)]};
 // //console.log(image());
 
 // //diana will write code to update the logo
@@ -28,7 +30,7 @@ p.removeChild(changeLogo);
 
 var images = document.createElement('IMG'); 
 images.id = 'hplogo';
-images.setAttribute("src", "https://lh3.googleusercontent.com/RVtwSeu8ZWL-IjY0o6IVDzjQ01BEqNBGUFn0n73enrORfVFmpK6AGHFDWZ2nYKMGXIaI4FXBHqMvPah36UvPiTq3b2i0EZ1uIR377Aw=s660"); 
+images.setAttribute("src", image()); 
 p.appendChild(images); 
 
 //console.log(window.location.pathname); 
