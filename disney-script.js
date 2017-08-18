660c03f7e3e72462c13b15045b648a0b9d3751ba
dianax3dh <diananhoang@gmail.com>
2017-08-18 12:00:36 -0700

    update

--- disney-script.js
+++ disney-script.js
@@ -28,10 +28,9 @@ var logo1 = {img: 'https://farm5.staticflickr.com/4398/36468138522_eba45f500b_z.
 var logo2 = {img: "https://preview.ibb.co/jnE3SF/princes.png", color: '#cce0ff'};
 var logo3 = {img: "https://preview.ibb.co/gVeyuv/disneygals2.png", color: '#e5ccff'};
 var logo4 = {img: "https://preview.ibb.co/eYycMa/disney_201_clipart_png_37.png", color: "#f0c1e1"};
-
 var logo5 = {img: "https://preview.ibb.co/bKJiuv/castle.png", color: "#ffb3b3"};
 var logo6 = {img: "https://preview.ibb.co/jcNySF/inside_out_fan_art_16.png", color: "#c1f0c1"};
-var logo7 = {img: "https://image.ibb.co/dEqP1a/theincredibles.png", color: "#ffcc99"};
+var logo7 = {img: "https://image.ibb.co/kcZTSF/theincredibles.png", color: "#ffcc99"};
 var logo8 = {img: "https://image.ibb.co/mrkvbk/classic_clipart_pooh_16.png", color: "#ffd9b3"};
 
 //put all possible logo images in an array
